6f5c20ac7d11428f66bf90005be13b7cc3808cf9
Ritika-pal38 <palritika294@gmail.com>
2025-01-14 17:45:51 +0530

    Odd even code added

---
+++ Level1/OddEvenNumbers.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class OddEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Error: Please enter a natural number.");
+            return;
+        }
+
+        int[] oddNumbers = new int[(number / 2) + 1];
+        int[] evenNumbers = new int[(number / 2) + 1];
+        int oddIndex = 0;
+        int evenIndex = 0;
+
+        // Separate odd and even numbers
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0) {
+                evenNumbers[evenIndex++] = i;
+            } else {
+                oddNumbers[oddIndex++] = i;
+            }
+        }
+
+        // Display odd numbers
+        System.out.print("Odd numbers: ");
+        for (int i = 0; i < oddIndex; i++) {
+            System.out.print(oddNumbers[i] + " ");
+        }
+        System.out.println();
+
+        // Display even numbers
+        System.out.print("Even numbers: ");
+        for (int i = 0; i < evenIndex; i++) {
+            System.out.print(evenNumbers[i] + " ");
+        }
+        System.out.println();
+    }
+}
