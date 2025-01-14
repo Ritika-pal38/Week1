652c41eb06224489288eb35bcc416aa3d6cf8653
Ritika-pal38 <palritika294@gmail.com>
2025-01-14 17:27:07 +0530

    Armstrong number code added

---
+++ Level3/ArmstrongNumber.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class ArmstrongNumber{
+    public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		int sum = 0;
+		int originalNumber = number;
+		
+		
+		while(originalNumber!=0) {
+			 int lastDigit = originalNumber%10;
+			sum = sum + (lastDigit*lastDigit*lastDigit);
+			originalNumber = originalNumber/10;
+		}
+		
+		if(sum==number){
+			System.out.println("The number "+number+" is a armstrong number.");
+		}
+		else{
+			System.out.print("Not a Armstrong Number");
+		}
+		
+		
+		
+		
+		
+		sc.close();
+	}
+}
