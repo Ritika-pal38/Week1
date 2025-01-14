8b26fca93c22c37c25812a8df6a9ef5efa2828a6
Ritika-pal38 <palritika294@gmail.com>
2025-01-14 12:48:58 +0530

    Age code added

---
+++ Level1/Age.java
@@ -0,0 +1,15 @@
+ // Creating class with name Age to find age of particular person name given
+ class Age{
+      
+	  public static void main(String[]args){
+	  
+	         String name = "Harry";  //Create variable of String type with name as name and assign value Harry   
+			 int birth_year = 2000;  //Create variable of int type with name as birth_year and assign value 2000
+			 int current_year = 2024; //Create variable of int type with name as current_year and assign value 2024
+			 int present_age = current_year - birth_year;  // Create variable of int type with name as present_age to find the age
+			 
+			 System.out.println("Harry's age in 2024 is  "+present_age);
+			 
+			 }
+		}
+			 
