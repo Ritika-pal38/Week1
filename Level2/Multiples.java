54e6376278324db93146fd27eadbdb4c2703970a
Ritika-pal38 <palritika294@gmail.com>
2025-01-14 17:23:07 +0530

    Multiple code added

---
+++ Level2/Multiples.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Multiples{
+    public static void main(String[]args){
+	  Scanner sc = new Scanner(System.in);
+	  int number = sc.nextInt();
+	  
+	  
+	  for(int i =1 ;i<100;i++){
+	     if(i%number==0){
+		 
+	 System.out.println(i);
+	 continue;
+	 }
+	 }
+	 
+	 sc.close();
+	 }
+	}
