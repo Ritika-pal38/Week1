525aee54d813c3b720c4f43c2de81a1bede8ca30
Ritika-pal38 <palritika294@gmail.com>
2025-01-14 12:53:04 +0530

    Conversion code added

---
+++ Level1/Conversion.java
@@ -0,0 +1,12 @@
+//Create class Conversion to convert km to miles
+class Conversion{
+
+public static void main(String[]args){
+       
+	   double distance_km = 10.8;  // Create double type variable with name as distance_km to store distance in kilometer and assign value 10.8
+	   double distance_miles = distance_km * 1.6 ;   //Create double type variable with name as distance_miles to convert km to miles
+	   
+	   System.out.println("The distance "+distance_km+" km in miles is "+distance_miles); // to print output
+	   
+	   }
+	 }
