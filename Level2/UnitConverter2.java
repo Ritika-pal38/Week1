7865a9197004e299e2058e1e8ac927b6a4cf0c92
Ritika-pal38 <palritika294@gmail.com>
2025-01-14 22:24:45 +0530

    Converter2 code added

---
+++ Level2/UnitConverter2.java
@@ -0,0 +1,41 @@
+public class UnitConverter {
+
+    // Method to convert yards to feet
+    public static double convertYardsToFeet(double yards) {
+        double yards2feet = 3;
+        return yards * yards2feet;
+    }
+
+    // Method to convert feet to yards
+    public static double convertFeetToYards(double feet) {
+        double feet2yards = 0.333333;
+        return feet * feet2yards;
+    }
+
+    // Method to convert meters to inches
+    public static double convertMetersToInches(double meters) {
+        double meters2inches = 39.3701;
+        return meters * meters2inches;
+    }
+
+    // Method to convert inches to meters
+    public static double convertInchesToMeters(double inches) {
+        double inches2meters = 0.0254;
+        return inches * inches2meters;
+    }
+
+    // Method to convert inches to centimeters
+    public static double convertInchesToCentimeters(double inches) {
+        double inches2cm = 2.54;
+        return inches * inches2cm;
+    }
+
+    public static void main(String[] args) {
+        // Test the methods
+        System.out.println("10 yards to feet: " + convertYardsToFeet(10));
+        System.out.println("30 feet to yards: " + convertFeetToYards(30));
+        System.out.println("5 meters to inches: " + convertMetersToInches(5));
+        System.out.println("12 inches to meters: " + convertInchesToMeters(12));
+        System.out.println("15 inches to centimeters: " + convertInchesToCentimeters(15));
+    }
+}
