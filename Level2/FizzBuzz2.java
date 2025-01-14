84f5d3a6e12f0dff3824f6ee06b02b0a0e1d01a4
Ritika-pal38 <palritika294@gmail.com>
2025-01-14 17:18:16 +0530

    FizzBuzz 2 code added

---
+++ Level2/FizzBuzz2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+  public class FizzBuzz2{
+     
+	 public static void main(String[]args){
+	 
+	 Scanner sc = new Scanner(System.in);
+	 int number = sc.nextInt();
+	 int i = 1;
+	 if(number>0){
+	 while(i<=number){
+	 if(i%3==0 && i%5==0){
+	  System.out.println("FizzBuzz");
+	  }
+	 else if(i%5==0){
+	  System.out.println("Buzz");
+	  }
+	 else if(i%3 ==0){
+	  System.out.println("Fizz");
+	  }
+	 else{
+	  System.out.println(i);
+	  }
+	  i++;
+	 }
+	 }
+	 sc.close();
+	 }
+	}
+	
